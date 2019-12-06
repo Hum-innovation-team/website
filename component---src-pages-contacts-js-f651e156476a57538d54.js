4e32d550e736db9d92c5fec48c3542c4fe6e4993
Giacomo Cerquone <cerquone@gmail.com>
2019-12-06 20:52:20 +0100

    Updates

---
+++ component---src-pages-contacts-js-f651e156476a57538d54.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{zAea:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("Zttt"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-contacts-js-f651e156476a57538d54.js.map
---
+++ styles-2b25e0f5b92eca897ef1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{EDuE:function(n,i,o){},EUrj:function(n,i,o){},iiPH:function(n,i,o){}}]);
+//# sourceMappingURL=styles-2b25e0f5b92eca897ef1.js.map
